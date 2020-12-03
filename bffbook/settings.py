08bc32914b4de929421a54a781f359f613556450
hlyndon12 <53565325+hlyndon12@users.noreply.github.com>
2020-12-03 16:33:01 +0530

    l

--- bffbook/settings.py
+++ bffbook/settings.py
@@ -148,7 +148,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static_project')
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn/static_root")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
+MEDIA_ROOT = os.path.join(BASE_DIR, "static_cdn/media_root")
